54ef0436e52d65a5623a844fe616371e5ba8ea24
Benjamin Wrist Lam <Benjaminlam@me.com>
2018-10-20 13:00:46 +0200

    Add project files.

---
+++ HackerRank/SockMerchant.cs
@@ -0,0 +1,55 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class SockMerchant
+{
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar)
+    {
+        var colors = new HashSet<int>();
+        var temp = 0;
+        var pairs = 0;
+        for (int i = 0; i < ar.Length; i++)
+        {
+            temp = ar[i];
+            if (!colors.Contains(temp))
+            {
+                colors.Add(temp);
+            }
+            else
+            {
+                pairs++;
+                colors.Remove(temp);
+            }
+
+        }
+
+        return pairs;
+    }
+
+    static void Main(string[] args)
+    {
+        
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+
+        var result = sockMerchant(n, ar);
+        Console.WriteLine(result);
+        Console.ReadLine();
+
+    }
+}
